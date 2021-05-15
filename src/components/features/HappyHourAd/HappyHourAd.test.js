b9d37a7b19600d792b7ee33818a73c4275b2be47
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-05-15 15:24:07 +0400

    TDD 7 20.2

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -99,3 +99,10 @@ describe('Component HappyHourAd shows promoDescription', () => {
 });
 
 
+describe('Component HappyHourAd with mocked Date and delay', () => {
+  checkDescriptionAfterTime('11:59:58', 1, '1');
+  checkDescriptionAfterTime('12:00:00', 2, mockProps.promoDescription);
+  checkDescriptionAfterTime('12:59:58', 1, mockProps.promoDescription);
+  checkDescriptionAfterTime('13:00:00', 60 * 60, 22 * 60 * 60 + '');
+});
+
