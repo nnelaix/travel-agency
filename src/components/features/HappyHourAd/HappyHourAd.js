1f3c41a4ba720bd7068aad2eb90a1327c0da5a39
Agata Lewandowska <lewandowskaagata15@gmail.com>
2021-05-15 15:07:19 +0400

    TDD 5 20.2

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -3,6 +3,17 @@ import styles from './HappyHourAdd.scss';
 import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
+
+  constructor(){
+    super();
+  
+    /* run this.forceUpdate() every second */
+    setInterval(() => {
+      this.forceUpdate();
+    },
+    1000
+    );
+  }
   render() {
     const {title} = this.props; 
 
